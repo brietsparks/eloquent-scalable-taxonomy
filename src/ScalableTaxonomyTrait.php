84503bee097a6211d15bc3a07e08ed287dee5d42
bsapaka <bws46@cornell.edu>
2016-03-28 23:24:19 -0500

    working, beta

--- src/ScalableTaxonomyTrait.php
+++ src/ScalableTaxonomyTrait.php
@@ -1,7 +1,9 @@
 <?php namespace Bsapaka\ScalableTaxonomy;
 
+use Bsapaka\EloquentAttribute\AttributeList;
 use Bsapaka\EloquentAttribute\AttributesTrait;
 use Nanigans\SingleTableInheritance\SingleTableInheritanceTrait;
+use Illuminate\Support\Facades\Validator;
 
 trait ScalableTaxonomyTrait {
 
@@ -9,30 +11,189 @@ trait ScalableTaxonomyTrait {
 	use SingleTableInheritanceTrait;
 
 	/**
+	 * @var AttributeList
+	 */
+	protected $fullAttributeList;
+
+
+	/**
+	 * @return AttributeList
+	 */
+	public function getAttributeList() {
+		return $this->fullAttributeList;
+	}
+
+	/**
+	 * Find the leaf class and populate the AttributeList with real values for all attributes in lineage
+	 *
 	 * @param array $attributes
 	 */
 	public function hydrateAttributeList(array $attributes) {
-		$typeField = static::$singleTableTypeField;
+		$this->fullAttributeList = AttributeList::make(
+			$this->lineageAttributes(get_called_class())
+		)->hydrate($attributes);
+	}
 
-		$classType = isset($attributes[$typeField]) ? $attributes[$typeField] : null;
-
-		if ($classType !== null) {
-			$childTypes = static::getSingleTableTypeMap();
-			if (array_key_exists($classType, $childTypes)) {
-				$class = $childTypes[$classType];
-			}
-		}
-
+	/**
+	 * Return the defined attributes of the class and its ancestors
+	 *
+	 * @param $class
+	 * @return array
+	 */
+	public static function lineageAttributes($class) {
+		//get all ancestors
 		$classes = class_parents($class);
 
-		$attributes = array();
+		//setup AttributeList that includes attributes of ancestor classes
+		$attributes = array_merge(array(),$class::defineAttributes());
 		foreach($classes as $class) {
 			if(method_exists($class, 'defineAttributes')) {
 				$attributes = array_merge($attributes, $class::defineAttributes());
 			}
 		}
 
-		static::getAttributeList()->add($attributes);
+		return $attributes;
+	}
+
+	/**
+	 * @return AttributeList
+	 */
+	public function attributeTemplate() {
+		return new AttributeList(static::lineageAttributes(get_class($this)));
+	}
+
+	/**
+	 * Get all leaves in the taxonomy
+	 *
+	 * @param bool|TRUE $fullyQualified
+	 * @return array
+	 */
+	public static function leafClasses($fullyQualified = true) {
+		$leafClasses = static::getSingleTableTypeMap();
+
+		if($fullyQualified) {
+			return $leafClasses;
+		} else {
+			return array_map('strtolower', array_keys($leafClasses));
+		}
+	}
+
+
+	/**
+	 * Return an array of all classes in the taxonomy
+	 *
+	 * @param bool|TRUE $fullyQualified
+	 * @return array
+	 */
+	public static function allClasses($fullyQualified = true) {
+		$taxons = array();
+
+		//get all leaves
+		$leaves = static::getSingleTableTypeMap();
+
+		//put in array the ancestor classes of each leaf
+		foreach($leaves as $className => $classPath) {
+			$taxons[$className] = $classPath;
+			foreach(class_parents($classPath) as $parentPath) {
+				$r = new \ReflectionClass($parentPath);
+				$parentName = strtolower($r->getShortName());
+				if(!in_array($parentPath, $taxons) && !$r->isAbstract()) {
+					$taxons[$parentName] = $parentPath;
+				}
+			}
+		}
+
+		if($fullyQualified) {
+			return $taxons;
+		} else {
+			return array_map('strtolower', array_keys($taxons));
+		}
+	}
+
+
+	/**
+	 * Get subclasses of the class
+	 * @return array
+	 */
+	public static function subClasses($fullyQualified = true) {
+		$subClasses = array();
+
+		$isfLeaf = property_exists(get_called_class(),'singleTableType');
+
+		//if not a leaf, then return the subclasses
+		if(!$isfLeaf) {
+			foreach(static::$singleTableSubclasses as $classPath) {
+				$r = new \ReflectionClass($classPath);
+				$className = strtolower($r->getShortName());
+				$subClasses[$className] = $classPath;
+			}
+		}
+
+		if($fullyQualified) {
+			return $subClasses;
+		} else {
+			return array_map('strtolower', array_keys($subClasses));
+		}
+	}
+
+	/**
+	 * Return an instance of a class in the taxonomy given its class name
+	 *
+	 * @param string $class
+	 * @return mixed
+	 */
+	public static function instanceOfType($class) {
+		$classes = static::allClasses();
+		$class = $classes[$class];
+		return new $class;
+	}
+
+
+	/**
+	 * Get the classes of a class in the taxonomy given its class name
+	 *
+	 * @param $class
+	 * @param bool|TRUE $fullyQualified
+	 * @return mixed
+	 */
+	public static function subClassesOfType($class, $fullyQualified = true) {
+		$classes = static::allClasses();
+		$class = $classes[$class];
+		return $class::subClasses($fullyQualified);
+	}
+
+
+
+	#region validation
+	protected $errors;
+
+	/**
+	 * @return bool
+	 */
+	public function hasValidData() {
+		$v = Validator::make($this->getAttributes(), $this->validationRules());
+
+		if ($v->fails()) {
+			$this->errors = $v->errors();
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function validationRules() {
+		return $this->attributeTemplate()->validationRules();
 
 	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getErrors() {
+		return $this->errors;
+	}
+	#endregion
+
 }
